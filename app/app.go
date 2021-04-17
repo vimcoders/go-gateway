4942d55af8e2f8495f1000fe7208eff54ab08026
vimcoders <vimcoders@gmail.com>
2021-04-18 00:49:17 +0800

    Run Cost

--- app/app.go
+++ app/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"sync"
+	"time"
 
 	"github.com/vimcoders/go-driver"
 	"github.com/vimcoders/go-lib"
@@ -11,7 +12,19 @@ import (
 
 var logger driver.Logger
 
-func Listen() (err error) {
+func Listen(waitGroup *sync.WaitGroup) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			logger.Error("Listen %v", e)
+		}
+
+		if err != nil {
+			logger.Error("Listen %v", err)
+		}
+
+		waitGroup.Done()
+	}()
+
 	listener, err := net.Listen("tcp", ":8888")
 
 	if err != nil {
@@ -30,6 +43,8 @@ func Listen() (err error) {
 }
 
 func Run() {
+	now := time.Now()
+
 	sysLogger, err := lib.NewSyslogger()
 
 	if err != nil {
@@ -41,4 +56,10 @@ func Run() {
 	var waitGroup sync.WaitGroup
 
 	waitGroup.Add(1)
+
+	go Listen(&waitGroup)
+
+	logger.Info("Run Cost %v", time.Now().Sub(now))
+
+	waitGroup.Wait()
 }
