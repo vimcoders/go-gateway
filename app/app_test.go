74c7991eb5632c1140a86a39ad7aed32d006de47
vimcoders <vimcoders@gmail.com>
2021-07-19 12:41:19 +0000

    test for app

--- app/app_test.go
+++ app/app_test.go
@@ -9,20 +9,14 @@ import (
 	"net"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/vimcoders/go-driver"
-	"github.com/vimcoders/go-lib"
 )
 
 func TestMain(m *testing.M) {
 	fmt.Println("begin")
-	sysLogger, err := lib.NewSyslogger()
-
-	if err != nil {
-		panic(err)
-	}
-
-	logger = sysLogger
+	go Run()
 
 	m.Run()
 	fmt.Println("end")
@@ -33,6 +27,7 @@ func TestLogin(t *testing.T) {
 
 	for i := 0; i < 10000; i++ {
 		waitGroup.Add(1)
+		time.Sleep(time.Second)
 
 		t.Run(fmt.Sprintf("case %v", i), func(t *testing.T) {
 			defer waitGroup.Done()
