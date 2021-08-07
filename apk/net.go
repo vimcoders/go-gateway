7d6f61440d967082db426a1f6db193648c7abbf9
vimcoders <vimcoders@gmail.com>
2021-08-07 12:39:20 +0800

    net

--- apk/net.go
+++ apk/net.go
@@ -2,8 +2,6 @@ package apk
 
 import (
 	"bufio"
-	"crypto/rand"
-	"crypto/rsa"
 	"io"
 	"net"
 	"time"
@@ -11,27 +9,37 @@ import (
 
 type Writer struct {
 	net.Conn
-	*Buffer
+	*bufio.Writer
 	t time.Duration
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
+	defer func() {
+		if err := w.Writer.Flush(); err != nil {
+			logger.Error("writer %v", err)
+		}
+	}()
+
 	length := len(p)
 
-	buf := w.Take(length + 2)
-	buf[0] = uint8(length >> 8)
-	buf[1] = uint8(length)
-	copy(buf[2:], p)
+	var header [2]byte
+
+	header[0] = uint8(length >> 8)
+	header[1] = uint8(length)
 
 	if err := w.SetWriteDeadline(time.Now().Add(w.t)); err != nil {
 		return 0, err
 	}
 
-	return w.Conn.Write(buf)
+	if n, err := w.Writer.Write(header[:]); err != nil {
+		return n, err
+	}
+
+	return w.Writer.Write(p)
 }
 
 func NewWriter(c net.Conn) io.Writer {
-	return &Writer{c, NewBuffer(), time.Second * 15}
+	return &Writer{c, bufio.NewWriter(c), time.Second * 15}
 }
 
 type Reader struct {
@@ -62,76 +70,4 @@ func (r *Reader) Read() (p []byte, err error) {
 
 func NewReader(c net.Conn) *Reader {
 	return &Reader{c, bufio.NewReaderSize(c, 512), time.Second * 5}
-}
-
-type Encoder struct {
-	*Writer
-	key *rsa.PublicKey
-}
-
-func (e *Encoder) Write(p []byte) (n int, err error) {
-	b, err := rsa.EncryptPKCS1v15(rand.Reader, e.key, p)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return e.Writer.Write(b)
-}
-
-func NewEncoder(c net.Conn, k *rsa.PublicKey) io.Writer {
-	return &Encoder{
-		Writer: &Writer{c, NewBuffer(), time.Second * 5},
-		key:    k,
-	}
-}
-
-type Decoder struct {
-	*Reader
-	key *rsa.PrivateKey
-}
-
-func (d *Decoder) Read() (p []byte, err error) {
-	b, err := d.Reader.Read()
-
-	if err != nil {
-		return nil, err
-	}
-
-	buf, err := rsa.DecryptPKCS1v15(rand.Reader, d.key, b)
-
-	if err != nil {
-		return
-	}
-
-	if _, err := d.Discard(len(b)); err != nil {
-		return nil, err
-	}
-
-	return buf, nil
-}
-
-func NewDecoder(c net.Conn, k *rsa.PrivateKey) *Decoder {
-	return &Decoder{
-		Reader: &Reader{c, bufio.NewReaderSize(c, 512), time.Second * 5},
-		key:    k,
-	}
-}
-
-type Buffer struct {
-	b []byte
-}
-
-func (b *Buffer) Take(n int) (p []byte) {
-	if n > len(b.b) {
-		return make([]byte, n)
-	}
-
-	return b.b[:n]
-}
-
-func NewBuffer() *Buffer {
-	return &Buffer{
-		b: make([]byte, 512),
-	}
 }
